3ff3e8529f69b1905858b8b10642242be73945c0
Jeff Thompson <mail@jeffreythompson.org>
2018-09-16 10:08:33 -0400

    Added a test sketch
    ...so I don't have to keep creating one to take a screenshot :)

---
+++ DarkThemeTestCode/DarkThemeTestCode.ino
@@ -0,0 +1,22 @@
+
+// a comment
+/* another comment */
+// a url: http://www.jeffreythompson.org
+
+int i = 100;
+char c = 'c';
+String s = "string";
+boolean y = true;
+
+void setup() {
+  pinMode(13, INPUT);
+
+  if (y == true) {
+    s = "hello world!";
+  }
+}
+
+void loop() {
+
+}
+
